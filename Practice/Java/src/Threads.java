2bc5e4dcdedc426d8d84ef566e66a8126e45ab9c
Sylvia Engmann <d3vg0d355@theMambaMentality.local>
2021-03-09 07:58:50 +0000

    some tutorials

---
+++ Practice/Java/src/Threads.java
@@ -0,0 +1,11 @@
+public class Threads implements Runnable {
+    @Override
+    public void run() {
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
